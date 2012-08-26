fbe4ece7cb0c24d028bd45f21b387542eb7f8503
Jay McCarthy <jay.mccarthy@gmail.com>
2012-08-25 21:50:18 -0600

    use logging and sorting

--- client.rkt
+++ client.rkt
@@ -2,13 +2,25 @@
 (require racket/cmdline
          racket/tcp
          racket/match
-         racket/system)
+         racket/file
+         racket/system
+         racket/runtime-path)
+
+(define-runtime-path log-file-path "sounds.log")
+(unless (file-exists? log-file-path)
+  (write-to-file (hash) log-file-path))
+
+(define (sound-uses sound)
+  (hash-ref (file->value log-file-path) sound 0))
 
 (define (client-connect server port dir)
   (define-values (from to) (tcp-connect server port))
   (define sounds 
-    (sort (map path->string (directory-list dir))
-          string-ci<=?))
+    (sort
+     (sort (map path->string (directory-list dir))
+           string-ci<?)
+     >
+     #:key sound-uses))
 
   (write sounds to)
   (flush-output to)
@@ -19,6 +31,13 @@
        (error 'client "Server disconnected")]
       [(? string? some-sound)
        (when (member some-sound sounds)
+         (write-to-file
+          #:exists 'replace
+          (hash-update (file->value log-file-path)
+                       some-sound
+                       add1
+                       0)
+          log-file-path)
          (define-values (sb stdout stdin stderr)
            (subprocess #f #f #f 
                        "/usr/bin/mplayer"
